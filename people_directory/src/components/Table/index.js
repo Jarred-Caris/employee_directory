4acf350e4f0d96539799f9e2ef436bc1afb18916
jarred <jarred.caris@gmail.com>
2021-05-02 12:46:51 +0930

    fixed errors in table as was not returning anything to browser

--- people_directory/src/components/Table/index.js
+++ people_directory/src/components/Table/index.js
@@ -1,70 +1,55 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./style.css";
-import API from "../utils/API";
-
-
+import API from "../../utils/API";
 
 class Table extends Component {
-    state = {
-      image: {},
-      name: "",
-      phone: Number,
-      email: "",
-      dob: Date,
-      
-    };
+  state = {
+    employees: [],
+  };
+  componentDidMount() {
+    this.employees();
+  }
 
-     
-    componentDidMount() {
-        this.employees;
-      }
+  employees() {
+    API.getEmployees()
+      .then((res) => this.setState({ employees: res.data.results }))
+      .catch((err) => console.log(err));
+  }
 
-      employees () {
-        API.getEmployees()
-          .then(res => this.setState({ res }))
-          .catch(err => console.log(err));
-        
-      };
-      
-
-      render() {
-        return (
-            <table class="table table-striped table-dark">
-            <thead>
-              <tr>
-                <th scope="col">Image</th>
-                <th scope="col">Name</th>
-                <th scope="col">Phone</th>
-                <th scope="col">Email</th>
-                <th scope="col">DOB</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
-              </tr>
-            </tbody>
-          </table>
-        )
-
-        
-      }
-        
-    
-
+  render() {
+    return (
+      <table class="table table-striped table-dark">
+        <thead>
+          <tr>
+            <th scope="col">Image</th>
+            <th scope="col">Name</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Email</th>
+            <th scope="col">DOB</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td>Larry</td>
+            <td>the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+}
 export default Table;
--- people_directory/src/utils/API.js
+++ people_directory/src/utils/API.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
-// Export an object containing methods we'll use for accessing the Dog.Ceo API
+
 
 export default {
-  getEmployee: function() {
+  getEmployees: function() {
     return axios.get("https://randomuser.me/api/?results=200");
   },
 
 };
+
